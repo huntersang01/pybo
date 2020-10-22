8819f3bc232f8f7dac38c00a185e8722bde73d92
huntersang01 <sin99027@gmail.com>
2020-10-23 01:07:48 +0900

    create account finish,login form add

--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -6,16 +6,17 @@ from pybo import db
 from pybo.forms import UserCreateForm
 from pybo.models import User
 
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/signup', methods=('GET','POST'))
+@bp.route('/signup/', methods=('GET','POST'))
 def signup():
     form = UserCreateForm()
     if request.method == 'POST' and form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if not user:
             user = User(username=form.username.data,
-                        password=generate_password_hash(form.password1.data),#복호화 불가능
+                        password=generate_password_hash(form.password1.data),
                         email=form.email.data)
             db.session.add(user)
             db.session.commit()
@@ -23,3 +24,7 @@ def signup():
         else:
             flash('이미 존재하는 사용자입니다.')
     return render_template('auth/signup.html', form=form)
+
+@bp.route('/login/', methods('GET','POST'))
+def login():
+    form = UserLoginForm()
