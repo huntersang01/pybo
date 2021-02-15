8861740ebeee0af8999841528ba4f8649ab691b0
huntersang01 <sin99027@gmail.com>
2021-02-16 00:26:14 +0900

    search add

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -5,7 +5,7 @@ from pybo.views.auth_views import login_required
 from werkzeug.utils import redirect
 
 from .. import db
-from ..models import Question
+from ..models import Question, Answer, User
 from ..forms import QuestionForm, AnswerForm
 bp = Blueprint('question', __name__, url_prefix='/quesiton')
 
@@ -13,11 +13,30 @@ bp = Blueprint('question', __name__, url_prefix='/quesiton')
 
 @bp.route('/list/')
 def _list():
+    # 입력 파라미터
     page = request.args.get('page', type=int, default=1) #페이지 get방식으로 요청한 url에서 page값 가져올때 사용
+    kw = request.args.get('kw', type=str, default='')
+
+    # 조회
     question_list = Question.query.order_by(Question.create_date.desc())
      #order_by는 조회결과를 정렬하는 함수 Question.create_date.desc()는 조회된 데이터를 작성일시 기준으로 역순정렬
+    if kw:
+
+        search = '%%{}%%'.format(kw)
+        sub_query = db.session.query(Answer.question_id, Answer.content, User.username) \
+            .join(User, Answer.user_id == User.id).subquery()
+        question_list = question_list \
+            .join(User) \             
+            .outerjoin(sub_query, sub_query.c.question_id == Question.id) \            
+            .filter(Question.subject.ilike(search) |  # 질문제목
+                    Question.content.ilike(search) |  # 질문내용
+                    User.username.ilike(search) |  # 질문작성자
+                    sub_query.c.content.ilike(search) |  # 답변내용
+                    sub_query.c.username.ilike(search)  # 답변작성자
+                    ) \
+            .distinct()
     question_list = question_list.paginate(page, per_page=10)
-    return render_template('question/question_list.html', question_list=question_list)
+    return render_template('question/question_list.html', question_list=question_list, page=page, kw=kw)
         #question/question_list.html 파일을 템플릿 파일이라 부름 템플릿파일을 화면으로 렌더링해줌
 
 @bp.route('/detail/<int:question_id>/')
